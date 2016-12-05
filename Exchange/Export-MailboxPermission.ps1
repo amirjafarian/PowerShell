d25cbf4ca661629b0d130ae869b6e2b0678c5229
amirjafarian <amir.jafarian@outlook.com>
2016-12-06 09:01:12 +1100

    Add files via upload

---
+++ Exchange/Export-MailboxPermission.ps1
@@ -0,0 +1,53 @@
+﻿#$env:PSModulePath
+$domainName = $env:USERDOMAIN
+$excludedUsers = @("NT AUTHORITY\SELF","$domainName\Domain Admins","DiscoverySearch")
+
+$mailboxes = Get-Mailbox 
+
+foreach($mbox in $mailboxes)
+{
+    $userID = $mbox.UserPrincipalName
+    $mailboxGUID = $mbox.ExchangeGuid
+    if($mbox.UserPrincipalName -eq $null -or $mbox.UserPrincipalName -eq "")
+    {
+        $userID = $mbox.Alias
+        #Write-Host $upn
+    }
+
+    try
+    {
+        $permission = Get-MailboxPermission -Identity "$mailboxGUID" | where {$_.IsInherited -eq $false -and  $excludedUsers -notcontains $_.User -and $_.Deny -eq $false}
+
+    }
+    catch 
+    {
+        Write-Host "$_ : $userID "
+    }
+
+
+    foreach ($acl in $permission)
+    {
+        if ($acl.User -notlike "S-1-5-21-*")
+        {
+            
+            $GrantedUserUPN = Get-Mailbox -Identity "$($acl.User)" -ErrorAction SilentlyContinue
+            
+            if($GrantedUserUPN -ne "" -and $GrantedUserUPN -ne $null)
+            {
+                $objAcl = @{
+                    UserName = $userID  
+                    Email = $mbox.PrimarySmtpAddress
+                    GrantedUser = $acl.User
+                    GrantedUserUPN = $GrantedUserUPN.PrimarySmtpAddress
+                    AccessRight = $acl.AccessRights[0]
+
+                }
+
+
+                $obj = New-Object -TypeName PSObject -Property $objAcl
+                Write-Output $obj        
+            }
+        }
+    }
+
+}
---
+++ Exchange/Export-SendASPermission.ps1
@@ -0,0 +1,58 @@
+﻿
+#$env:PSModulePath
+$domainName = $env:USERDOMAIN
+$excludedUsers = @("NT AUTHORITY\SELF","$domainName\Domain Admins","DiscoverySearch")
+
+$mailboxes = Get-Mailbox 
+
+foreach($mbox in $mailboxes)
+{
+    $userID = $mbox.UserPrincipalName
+    $SamAccountName = $mbox.SamAccountName
+    $DispName = $mbox.DisplayName
+    $mailboxGUID = $mbox.ExchangeGuid
+    $DN = $mbox.DistinguishedName
+    if($mbox.UserPrincipalName -eq $null -or $mbox.UserPrincipalName -eq "")
+    {
+        $userID = $mbox.Alias
+        #Write-Host $upn
+    }
+
+    try
+    {
+#        Get-ADPermission | where {($_.ExtendedRights -like “*Send-As*”) -and ($_.IsInherited -eq $false) -and -not ($_.User -like “NT AUTHORITY\SELF”)} 
+        $permission = Get-ADPermission -Identity "$DN" | where {($_.ExtendedRights -like “*Send-As*”) -and $_.IsInherited -eq $false -and  $excludedUsers -notcontains $_.User}
+
+    }
+    catch 
+    {
+        Write-Host "$_ : $userID "
+    }
+
+
+    foreach ($acl in $permission)
+    {
+        if ($acl.User -notlike "S-1-5-21-*")
+        {
+            
+            $GrantedUserUPN = Get-Mailbox -Identity "$($acl.User)" -ErrorAction SilentlyContinue
+            
+            if($GrantedUserUPN -ne "" -and $GrantedUserUPN -ne $null)
+            {
+                $objAcl = @{
+                    UserName = $userID  
+                    Email = $mbox.PrimarySmtpAddress
+                    GrantedUser = $acl.User
+                    GrantedUserUPN = $GrantedUserUPN.PrimarySmtpAddress
+                    AccessRight = $acl.AccessRights[0]
+
+                }
+
+
+                $obj = New-Object -TypeName PSObject -Property $objAcl
+                Write-Output $obj        
+            }
+        }
+    }
+
+}
